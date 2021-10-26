7be5ab83f3cd9ceab8f7e58497d3a4fbab7fb2db
babysuse <babysuse2018@gmail.com>
2021-10-26 18:08:21 -0400

    Modify findNextTerm()

--- src/query_processor.cpp
+++ src/query_processor.cpp
@@ -1,5 +1,8 @@
 #include "query_processor.h"
 #include "data_compress.h"
+#include <algorithm>
+#include <cctype>
+#include <cstddef>
 
 using namespace std;
 
@@ -17,48 +20,51 @@ vector<vector<string>> QueryProcessor::process(string query) {
     vector<vector<string>> groups;
     groups.push_back({});
 
-    int start = 0;
-    string term = findNextTerm(query, start);
+    size_t start = 0;
+    size_t end = 0;
+    string term = findNextTerm(query, start, end);
+    // TODO: add support for parenthesis in query
     while (term.size() > 0) {
         if (term.compare("OR") == 0) {
-            term = findNextTerm(query, start);
+            start = end;
+            term = findNextTerm(query, start, end);
             if (term.size() == 0) {
                 cout << "Invalid query" << endl;
                 return {};
             }
 
             // add to new group
+            transform(term.begin(), term.end(), term.begin(), ::tolower);
             groups.push_back({ term });
         } else {
             if (term.compare("AND") == 0) {
-                term = findNextTerm(query, start);
+                start = end;
+                term = findNextTerm(query, start, end);
                 if (term.size() == 0) {
                     cout << "Invalid query" << endl;
                     return {};
                 }
             }
+            transform(term.begin(), term.end(), term.begin(), ::tolower);
             groups[0].push_back(term);
         }
-        term = findNextTerm(query, start);
+        start = end;
+        term = findNextTerm(query, start, end);
     }
     return groups;
 }
 
 // helper of QueryProcessor::process()
-string QueryProcessor::findNextTerm(string& str, int& start) {
-    if (start >= str.size())
+string QueryProcessor::findNextTerm(string& str, size_t& start, size_t& end) {
+    if (start == string::npos || (start = str.find_first_not_of(" ", start)) == string::npos)
         return "";
 
-    string term;
-    int end = str.find(" ", start);
-    if (end == -1) {
-        term = str.substr(start);
-        start = str.size();
+    end = str.find_first_of(" ", start);
+    if (end == string::npos) {
+        return str.substr(start);
     } else {
-        term = str.substr(start, end - start);
-        start = end + 1;
+        return str.substr(start, end - start);
     }
-    return term;
 }
 
 void QueryProcessor::printInfo(string term) {
--- src/query_processor.h
+++ src/query_processor.h
@@ -11,7 +11,6 @@ class QueryProcessor {
     public:
         QueryProcessor();
         std::vector<std::vector<std::string>> process(std::string);
-
         void printInfo(std::string);
         void getDocs(std::string, std::vector<unsigned>);
         void getFreqs(std::string, std::vector<unsigned>);
@@ -19,7 +18,7 @@ class QueryProcessor {
         IndexReader::InvListMeta invlist;
         std::vector<DocMeta> docmeta;
 
-        std::string findNextTerm(std::string&, int&);
+        std::string findNextTerm(std::string&, size_t&, size_t&);
 };
 
 #endif
